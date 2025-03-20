09ad4e11361eff63ec904ba6006779857849bd4b
Nextpage <92269411+nxtpge@users.noreply.github.com>
2025-03-21 00:12:21 +0100

    fix(oracle): wrong test for JARO_WINKLER_SIMILARITY

--- tests/Query/Oracle/JaroWinklerSimilarityTest.php
+++ tests/Query/Oracle/JaroWinklerSimilarityTest.php
@@ -10,8 +10,8 @@ class JaroWinklerSimilarityTest extends OracleTestCase
     public function testJaroWinklerSimilarity(): void
     {
         $this->assertDqlProducesSql(
-            'SELECT UTL_MATCH.JARO_WINKLER_SIMILARITY(p.name, \'expr2\') FROM DoctrineExtensions\Tests\Entities\Product p',
-            'SELECT NVL2(p0_.name, \'expr2\') AS sclr_0 FROM Product p0_'
+            'SELECT JARO_WINKLER_SIMILARITY(p.name, \'expr2\') FROM DoctrineExtensions\Tests\Entities\Product p',
+            'SELECT UTL_MATCH.JARO_WINKLER_SIMILARITY(p0_.name, \'expr2\') AS sclr_0 FROM Product p0_'
         );
     }
 }
